63bbdbb2c184553b2c701394be6425a86951d6e6
Aleksandr Lakienko <sasha-lakienko5@mail.ru>
2022-10-28 14:29:01 +0300

    Задание 2
    Студенты могут ставить оценки лекторам, у лекторов есть словарь оценок за курсы которые они ведут, ревьюеры ставят оценки студентам

--- ООПдз.py
+++ ООПдз.py
@@ -10,6 +10,17 @@ class Student:
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)
 
+    def rate_lc(self, lecturer, course, grade):
+        if isinstance(lecturer,
+                      Lecturer) and course in lecturer.courses_attached \
+                and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -17,6 +28,14 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached \
                 and course in student.courses_in_progress:
@@ -28,28 +47,37 @@ class Mentor:
             return 'Ошибка'
 
 
-class Lector(Mentor):
-    pass
-
-
-class Reviewer(Mentor):
-    pass
-
-
-best_student = Student('Roy', 'Eman', 'your_gender')
-best_student.finished_courses += ['Git']
-best_student.courses_in_progress += ['Python']
-
-print(best_student.finished_courses)
-print(best_student.courses_in_progress)
-print(best_student.grades)
-
-cool_mentor = Mentor('Olga', 'Petrova')
-cool_mentor.courses_attached += ['Python']
-print(cool_mentor.courses_attached)
-
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-
-print(best_student.grades)
+# best_student = Student('Roy', 'Eman', 'your_gender')
+# best_student.finished_courses += ['Git']
+# best_student.courses_in_progress += ['Python',]
+#
+# print(best_student.finished_courses)
+# print(best_student.courses_in_progress)
+# print(best_student.grades)
+# print('1' * 10)
+#
+# cool_lecturer = Lecturer('Olga', 'Petrova')
+# cool_lecturer.courses_attached += ['Python']
+# cool_lecturer.courses_attached += ['Git']
+# print(cool_lecturer.courses_attached)
+# print('2' * 10)
+#
+# cool_reviewer = Reviewer('Vova', 'Topov')
+# cool_reviewer.courses_attached += ['Python']
+# cool_reviewer.rate_hw(best_student, 'Python', 10)
+# cool_reviewer.rate_hw(best_student, 'Python', 10)
+# cool_reviewer.rate_hw(best_student, 'Python', 10)
+#
+# print(best_student.grades)
+# print(cool_reviewer.__dict__)
+# print('3' * 10)
+#
+# print(best_student.grades)
+# print('4' * 10)
+#
+# best_student.rate_lc(cool_lecturer, 'Python', 5)
+# print(cool_lecturer.grades)
+# best_student.rate_lc(cool_lecturer, 'Git', 8)
+#
+# print(cool_lecturer.grades)
+# print(cool_lecturer.__dict__)
