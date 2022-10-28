5c2ab9ce1122851755e952c31f5ddce267682b79
Aleksandr Lakienko <sasha-lakienko5@mail.ru>
2022-10-28 17:33:58 +0300

    Задание 4 вызов функций
    Вызваны все функции.
    Добавлено округление значений среднего балла. Переименован список значений в методах average.

--- ООПдз.py
+++ ООПдз.py
@@ -22,16 +22,16 @@ class Student:
             return 'Ошибка'
 
     def _average_grade_hw(self):
-        list = []
+        list_1 = []
         for value in self.grades.values():
             for item in value:
-               list.append(item)
-        avrg = sum(list) / len(list)
+                list_1.append(item)
+        avrg = sum(list_1) / len(list_1)
         return avrg
 
     def __str__(self):
         res = f'Имя: {self.name}\nФамилия: {self.surname}' \
-              f'\nСредняя оценка за домашние задания: {self._average_grade_hw()}' \
+              f'\nСредняя оценка за домашние задания: {round(self._average_grade_hw(), 2)}' \
               f'\nКурсы в процессе изучения: {", ".join(self.courses_in_progress)}' \
               f'\nЗавершенные курсы: {", ".join(self.finished_courses)}'
         return res
@@ -56,17 +56,23 @@ class Lecturer(Mentor):
         self.grades = {}
 
     def _average_grade_lc(self):
-        list = []
+        list_1 = []
         for value in self.grades.values():
             for item in value:
-               list.append(item)
-        avrg = sum(list) / len(list)
+                list_1.append(item)
+        avrg = sum(list_1) / len(list_1)
         return avrg
 
     def __str__(self):
         res = f'Имя: {self.name}\nФамилия: {self.surname}' \
-              f'\nСредняя оценка за лекции: {self._average_grade_lc()} '
+              f'\nСредняя оценка за лекции: {round(self._average_grade_lc(), 2)}'
         return res
+
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Такого преподавателя нет!')
+            return
+        return self._average_grade_lc() < other._average_grade_lc()
 
 
 class Reviewer(Mentor):
@@ -83,10 +89,44 @@ class Reviewer(Mentor):
     def __str__(self):
         res = f'Имя: {self.name}\nФамилия: {self.surname}'
         return res
+# _______________________________________________________________________________
+student_1 = Student('Olga', 'Petrova', 'F')
+student_1.finished_courses += ['Git']
+student_1.courses_in_progress += ['Python']
+student_1.add_courses('Java')
 
-    def __lt__(self, other):
-        if not isinstance(other, Lecturer):
-            print('Такого преподавателя нет!')
-            return
-        return self._average_grade_lc() < other._average_grade_lc()
+student_2 = Student('Egor', 'Popov', 'M')
+student_2.finished_courses += ['SQL']
+student_2.courses_in_progress += ['Django']
+student_2.add_courses('Go')
 
+lecturer_1 = Lecturer('Anna', 'Sokolova')
+lecturer_1.courses_attached += ['Python']
+
+lecturer_2 = Lecturer('Oleg', 'Ivanov')
+lecturer_2.courses_attached += ['Django']
+
+reviewer_1 = Reviewer('Anton', 'Rogov')
+reviewer_1.courses_attached += ['Python']
+reviewer_1.rate_hw(student_1, 'Python', 5)
+reviewer_1.rate_hw(student_1, 'Python', 8)
+reviewer_1.rate_hw(student_1, 'Python', 10)
+
+reviewer_2 = Reviewer('Ivan', 'Orlov')
+reviewer_2.courses_attached += ['Django']
+reviewer_2.rate_hw(student_2, 'Django', 3)
+reviewer_2.rate_hw(student_2, 'Django', 10)
+reviewer_2.rate_hw(student_2, 'Django', 6)
+
+student_1.rate_lc(lecturer_1, 'Python', 10)
+student_1.rate_lc(lecturer_1, 'Python', 8)
+student_1.rate_lc(lecturer_1, 'Python', 8)
+
+student_2.rate_lc(lecturer_2, 'Django', 4)
+student_2.rate_lc(lecturer_2, 'Django', 8)
+student_2.rate_lc(lecturer_2, 'Django', 2)
+
+print(reviewer_1, reviewer_2, student_1, student_2, lecturer_1, lecturer_2, sep='\n\n')
+
+print(student_1 > student_2)
+print(lecturer_1 == lecturer_2)
